bc6aa92b6395cb7066111749896f4d2702f0193b
Erin Black <erin.black@fieldnation.com>
2018-07-18 15:28:53 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// let's go!
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker.js';
+import App from './components/App.js';
+import './css/style.css';
+
+render(<App />, document.querySelector('#main'));
+
