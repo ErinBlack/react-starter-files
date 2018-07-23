ed2cb016f1e414e5141851ed11c9b50a60e6209e
Erin Black <erin.black@fieldnation.com>
2018-07-23 16:47:22 -0500

    added onSubmit event for form on store picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from "react";
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (e) => {
+    // 1. Stop page from re-loading
+    e.preventDefault();
+    console.log('in goToStore');
+    // 2. get the text from input
+    console.log('this.myInput', this.myInput);
+    // 3. Change the page to / store/ whatever-the-entered
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input 
+          type="text" 
+          ref={this.myInput}
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
